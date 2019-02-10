ceeebce7de4f187bc2de1031f814bf1628431f9c
Dung Tran <dung.tran@furucrm.com>
2019-02-10 21:42:23 +0700

    import

---
+++ wp-config.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Cấu hình cơ bản cho WordPress
+ *
+ * Trong quá trình cài đặt, file "wp-config.php" sẽ được tạo dựa trên nội dung 
+ * mẫu của file này. Bạn không bắt buộc phải sử dụng giao diện web để cài đặt, 
+ * chỉ cần lưu file này lại với tên "wp-config.php" và điền các thông tin cần thiết.
+ *
+ * File này chứa các thiết lập sau:
+ *
+ * * Thiết lập MySQL
+ * * Các khóa bí mật
+ * * Tiền tố cho các bảng database
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** Thiết lập MySQL - Bạn có thể lấy các thông tin này từ host/server ** //
+/** Tên database MySQL */
+define('DB_NAME', 'd27q6k6t6t8di2');
+
+/** Username của database */
+define('DB_USER', 'lyadmllrefunkv');
+
+/** Mật khẩu của database */
+define('DB_PASSWORD', '76763d8a17fcb9126b325ffdb263c7e5296d7385e7d359c7bf817a5ff7415444');
+
+/** Hostname của database */
+define('DB_HOST', 'ec2-54-243-228-140.compute-1.amazonaws.com');
+
+/** Database charset sử dụng để tạo bảng database. */
+define('DB_CHARSET', 'utf8');
+
+/** Kiểu database collate. Đừng thay đổi nếu không hiểu rõ. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Khóa xác thực và salt.
+ *
+ * Thay đổi các giá trị dưới đây thành các khóa không trùng nhau!
+ * Bạn có thể tạo ra các khóa này bằng công cụ
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * Bạn có thể thay đổi chúng bất cứ lúc nào để vô hiệu hóa tất cả
+ * các cookie hiện có. Điều này sẽ buộc tất cả người dùng phải đăng nhập lại.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
+/**#@-*/
+
+/**
+ * Tiền tố cho bảng database.
+ *
+ * Đặt tiền tố cho bảng giúp bạn có thể cài nhiều site WordPress vào cùng một database.
+ * Chỉ sử dụng số, ký tự và dấu gạch dưới!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Dành cho developer: Chế độ debug.
+ *
+ * Thay đổi hằng số này thành true sẽ làm hiện lên các thông báo trong quá trình phát triển.
+ * Chúng tôi khuyến cáo các developer sử dụng WP_DEBUG trong quá trình phát triển plugin và theme.
+ *
+ * Để có thông tin về các hằng số khác có thể sử dụng khi debug, hãy xem tại Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* Đó là tất cả thiết lập, ngưng sửa từ phần này trở xuống. Chúc bạn viết blog vui vẻ. */
+
+/** Đường dẫn tuyệt đối đến thư mục cài đặt WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Thiết lập biến và include file. */
+require_once(ABSPATH . 'wp-settings.php');
